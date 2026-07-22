ad53ae16e9497fab30ecef27e0aa77e4038ca00c
EmmanuelAdu <christorenzo20@gmail.com>
2026-07-21 22:35:08 -0400

    Added a last item

---
+++ code_py/arrays_hashing/10_insert-value.py
@@ -0,0 +1,13 @@
+'''Given an array [5, 10, 15, 20]
+Insert 60 to array and sum
+'''
+
+A = [5, 10, 15, 20]
+A.insert(2, 60)
+print(A)
+
+
+# for last index or end of list use append
+
+A.append(40)
+print(A)
